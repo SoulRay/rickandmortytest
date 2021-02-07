52f8f797126be9348f53d65202b9aa5f90cfa0d5
SoulRay <c.rodriguez.r96@gmail.com>
2021-02-06 21:09:38 -0300

    add match letter in single page API

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 import Rick from "./modules/RickAndMorty.js";
-
+//console.time("t1");
 const RickAndMorty = new Rick();
 //debugger;
 RickAndMorty.getLocationName().then((data) => {
-    document.getElementById("locationCounter").innerHTML = "1";
+    const quantity = Object.values(data.results).reduce((a, n) => {
+        return a+ n.name.match(/l/ig).length;
+    }, 0);
+    document.getElementById("locationCounter").innerHTML = quantity;
 });
 
 RickAndMorty.getEpisodeName().then((data) => {
-    debugger
-    document.getElementById("episodeCounter").innerHTML = "2";
+    const quantity = Object.values(data.results).reduce((a, n) => {
+        return a+ n.name.match(/e/ig).length;
+    }, 0);
+    document.getElementById("episodeCounter").innerHTML = quantity;
 });
 
 RickAndMorty.getCharacterName().then((data) => {
-    document.getElementById("characterCounter").innerHTML = "3";
+    const quantity = Object.values(data.results).reduce((a, n) => {
+        return a+ n.name.match(/c/ig).length;
+    }, 0);
+    document.getElementById("characterCounter").innerHTML = quantity;
 });
+
+console.log(RickAndMorty.getCountPage());
+//console.timeEnd("t1");
