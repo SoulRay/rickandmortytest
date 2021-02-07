a6b1cf547d7da087ecc664a395e677dfc694dbb3
SoulRay <c.rodriguez.r96@gmail.com>
2021-02-07 00:32:31 -0300

    added call to new method from RickAndMorty class

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
 import Rick from "./modules/RickAndMorty.js";
-//console.time("t1");
+console.time("t1");
 const RickAndMorty = new Rick();
-//debugger;
-RickAndMorty.getLocationName().then((data) => {
-    const quantity = Object.values(data.results).reduce((a, n) => {
-        return a+ n.name.match(/l/ig).length;
-    }, 0);
-    document.getElementById("locationCounter").innerHTML = quantity;
-});
 
-RickAndMorty.getEpisodeName().then((data) => {
-    const quantity = Object.values(data.results).reduce((a, n) => {
-        return a+ n.name.match(/e/ig).length;
-    }, 0);
-    document.getElementById("episodeCounter").innerHTML = quantity;
-});
+const totalChar = async () => { 
+    document.getElementById("locationCounter").innerHTML = await RickAndMorty.getLetterQtyByName('location', 'l', 'l');
+    document.getElementById("episodeCounter").innerHTML = await RickAndMorty.getLetterQtyByName('episode', 'e', 'e');
+    document.getElementById("characterCounter").innerHTML = await RickAndMorty.getLetterQtyByName('character', 'c', 'c');
+};
 
-RickAndMorty.getCharacterName().then((data) => {
-    const quantity = Object.values(data.results).reduce((a, n) => {
-        return a+ n.name.match(/c/ig).length;
-    }, 0);
-    document.getElementById("characterCounter").innerHTML = quantity;
-});
-
-console.log(RickAndMorty.getCountPage());
-//console.timeEnd("t1");
+totalChar();
+console.timeEnd("t1");
