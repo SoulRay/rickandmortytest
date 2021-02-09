46e951850543e76dcfc8a9c2c1312a13dc0e3fec
SoulRay <c.rodriguez.r96@gmail.com>
2021-02-08 23:48:20 -0300

    call methods were modified

--- index.js
+++ index.js
@@ -1,34 +1,55 @@
 import Rick from "./modules/RickAndMorty.js";
-
 const RickAndMorty = new Rick();
 
-const totalChar = async () => { 
-    console.time("t1");
-    document.getElementById("locationCounter").innerHTML = await RickAndMorty.getLetterQtyByName('location', 'l', 'l');
-    document.getElementById("episodeCounter").innerHTML = await RickAndMorty.getLetterQtyByName('episode', 'e', 'e');
-    document.getElementById("characterCounter").innerHTML = await RickAndMorty.getLetterQtyByName('character', 'c', 'c');
-    console.timeEnd("t1");
+const charCounter = async () => { 
+    const begin = Date.now();
+    document.getElementById("charCount").innerHTML = `<div>Total letras L en Location: ${await RickAndMorty.getLetterQtyByName('location', 'l', 'l')}</div>
+                                                    <div>Total letras E en Episode: ${await RickAndMorty.getLetterQtyByName('episode', 'e', 'e')}</div>
+                                                    <div>Total letras C en Character: ${await RickAndMorty.getLetterQtyByName('character', 'c', 'c')}</div>`
+    const end = Date.now();
+    document.getElementById('execTimeOne').innerHTML = `Tiempo de ejecuci&oacute;n: ${(end-begin)/1000+" secs"}`;
 };
 
-totalChar();
+charCounter();
 
-
-const init = async () => {
-    console.time("t2");
-    debugger;
-    let newObject = {};
-    const location = await RickAndMorty.getAllResourceData('location');
-    const character = await RickAndMorty.getAllResourceData('character');
+const episodeLocation = async () => {
+    const begin = Date.now();    
     const episode = await RickAndMorty.getAllResourceData('episode');
-    newObject.totalEpisode = episode.length;
+    const character = await RickAndMorty.getAllResourceData('character');
+
     const episodeFilter = episode.map(x => {
-        //debugger;
-        const character2 = x.characters;
-        return {'episode': x.id ,character2};
+        const characterUrl = x.characters;
+
+        const returnOrigins = characterUrl.map(y => {
+            const characterFilter = character.filter(c => c.url == y);
+            return characterFilter[0].origin.name;
+        });
+
+        const origin = returnOrigins.filter((value, index, array) => index === array.indexOf(value));
+        return {'episode': x.id, 'originList' : origin};
     });
 
+    let table = `<table id='episodeTable'>`;
+    episodeFilter.map(x => {
+        return table += `
+                        <tr>
+                            <td>
+                                Episode: ${x.episode}
+                            </td>
+                            <td>
+                                Location Origin: ${x.originList.length}
+                                <br>
+                                ${'<li>'+x.originList.join().replaceAll(',', '<li>')}
+                            </td>
+                        </tr>`
+    });
 
-    console.timeEnd("t2");
+    table += `</table>`;
+    document.getElementById('table').innerHTML = table;
+    const end = Date.now();
+    document.getElementById('execTimeTwo').innerHTML = `Tiempo de ejecuci&oacute;n: ${(end-begin)/1000+" secs"}`;
 }
 
-init();
+episodeLocation();
+
+
